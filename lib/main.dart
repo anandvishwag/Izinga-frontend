5051ed625d6369088a05e1b8b4645706edcaf334
LAPTOP-ACR7TECJ\PK <pk3958@gmail.com>
2020-12-09 16:54:09 +0530

    two file changes

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-       title: 'Iz',
+       title: 'Izinga',
       theme: ThemeData(
         fontFamily: 'Fira Sans',
         primarySwatch: Colors.blue,
